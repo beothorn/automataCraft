9ef964b4ed61a069cdbe2eb940c7000456220ad0
Beothorn <lucass@gmail.com>
2021-05-21 15:37:17 +0200

    A block that runs on the x axis

--- src/main/java/br/com/isageek/voxsophon/VoxsophonTileEntity.java
+++ src/main/java/br/com/isageek/voxsophon/VoxsophonTileEntity.java
@@ -1,10 +1,12 @@
 package br.com.isageek.voxsophon;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class VoxsophonTileEntity extends TileEntity implements ITickableTileEntity {
     public VoxsophonTileEntity(TileEntityType<VoxsophonTileEntity> tileEntityType) {
@@ -14,7 +16,21 @@ public class VoxsophonTileEntity extends TileEntity implements ITickableTileEnti
     @Override
     public void tick() {
         BlockPos blockPos = getBlockPos();
-        setPosition(blockPos.offset(1, 0, 0));
-        //world set chunk somehow???
+        BlockPos newBlockPosition = blockPos.offset(1, 0, 0);
+        World world = getLevel();
+        BlockState otherBlock = world.getBlockState(newBlockPosition);
+        if(otherBlock.getBlock() == Blocks.AIR){
+            BlockState blockState = getBlockState();
+            world.setBlock(
+                    newBlockPosition,
+                    blockState,
+                    0,
+                    0
+            );
+            world.removeBlock(
+                    blockPos,
+                    true
+            );
+        }
     }
 }
